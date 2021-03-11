5dcb6d9538a76bfc1cf349e41c73b3e28caa960f
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-03-11 11:35:13 -1000

    Hidden api key

--- iss-tracker/src/components/Map.js
+++ iss-tracker/src/components/Map.js
@@ -1,55 +1,71 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+import './map.css'
 
-const ISS_URL = "http://api.open-notify.org/iss-now.json"
-const MAP_KEY = process.env.REACT_APP_MAP_KEY
-const img = <img src='./iss.svg' alt='iss' height='45px'/>
+const ISS_URL = "http://api.open-notify.org/iss-now.json";
+const MAP_KEY = process.env.REACT_APP_MAP_KEY;
+const img = <img src="./iss.svg" alt="iss" height="45px" />;
 
-const SpaceStation = ({ img }) => <div>{img}</div>
+const SpaceStation = ({ img }) => <div>{img}</div>;
 
 class Map extends Component {
- 
-  state ={
-    center:{
-      lat:0,
-      lng:0
-    },
-    zoom:1
-  }
-
-  componentDidMount(){
-   this.getCoordinates()
-   this.interval=setInterval(this.getCoordinates, 2000)
-}
-
-componentWillMount(){
-  clearInterval(this.interval)
-}
-
-getCoordinates = () => {
-  fetch(ISS_URL)
-  .then(res => res.json())
-  .then(data => this.setState({
+  state = {
     center: {
-      lat:data.iss_position.latitude,
-      lng:data.iss_position.longitude
-    }
-  }))
-}
-  
-  render(){
-    console.log("LAT:", this.state.center.lat)
-    console.log("LNG:", this.state.center.lng)
+      lat: 0,
+      lng: 0,
+    },
+    zoom: 1,
+  };
 
-    return(
+  componentDidMount() {
+    this.getCoordinates();
+    this.interval = setInterval(this.getCoordinates, 2000);
+  }
+
+  componentWillMount() {
+    clearInterval(this.interval);
+  }
+
+  getCoordinates = () => {
+    fetch(ISS_URL)
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          center: {
+            lat: data.iss_position.latitude,
+            lng: data.iss_position.longitude,
+          },
+        })
+      );
+  };
+
+  render() {
+    console.log("LAT:", this.state.center.lat);
+    console.log("LNG:", this.state.center.lng);
+
+    return (
       <div>
-        <p>latitude: {this.state.center.lat}</p>
-    <p>Longitude: {}</p>
+        <p className='coords'>latitude: {this.state.center.lat}</p>
+        <p className='coords'>Longitude: {this.state.center.lng}</p>
+        <div className='map' style={{height: '100vh', width: '100%'}}>
+          <GoogleMapReact className='map'
+          bootstrapURLKeys={{key: MAP_KEY}}
+          center={this.state.center}
+          zoom={this.state.zoom}
+          >
+
+         
+          <SpaceStation
+
+          lat = {this.state.center.lat}
+          lng = {this.state.center.lng}
+          img = {img}
+          />
+          </GoogleMapReact>
       </div>
-    )
+      </div>
+    );
   }
 }
-
-
 
 export default Map;
--- iss-tracker/src/components/Scroll.js
+++ iss-tracker/src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Scroll-able component
 const Scroll = (props) => {
   return (
-    <div style={{overflowY: 'scroll', border: '5px solid white', height: '800px'}}>
+    <div style={{overflowY: 'scroll', border: '5px solid white', height: '1000px'}}>
       {props.children}
       
     </div>
